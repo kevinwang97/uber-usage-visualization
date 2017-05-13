1e2ff344e8d96e2e275a6811d41ea68b8d1d8ee8
Nolan Dey <ndey96@gmail.com>
2017-05-13 11:08:16 -0400

    Add parsing code and jsonified geo

---
+++ maps/map.js
@@ -0,0 +1,63 @@
+
+// This example creates a simple polygon representing the Bermuda Triangle.
+// When the user clicks on the polygon an info window opens, showing
+// information about the polygon's coordinates.
+
+var map;
+var infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: {lat: 40.7058253, lng: -74.1180864},
+    mapTypeId: 'terrain'
+  });
+
+  // Define the LatLng coordinates for the polygon.
+  var triangleCoords = [
+      {lat: 40.63128415, lng: -73.97605079},
+      {lat: 40.6307549, lng: -73.97716655},
+      {lat: 40.62987974, lng: -73.97699992}
+  ];
+
+
+  // Construct the polygon.
+  var bermudaTriangle = new google.maps.Polygon({
+    paths: triangleCoords,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 3,
+    fillColor: '#000000',
+    fillOpacity: 0.35
+  });
+  bermudaTriangle.setMap(map);
+
+  // Add a listener for the click event.
+  bermudaTriangle.addListener('click', showArrays);
+
+  infoWindow = new google.maps.InfoWindow;
+}
+
+/** @this {google.maps.Polygon} */
+function showArrays(event) {
+  // Since this polygon has only one path, we can call getPath() to return the
+  // MVCArray of LatLngs.
+  var vertices = this.getPath();
+
+  var contentString = '<b>Bermuda Triangle polygon</b><br>' +
+      'Clicked location: <br>' + event.latLng.lat() + ',' + event.latLng.lng() +
+      '<br>';
+
+  // Iterate over the vertices.
+  for (var i =0; i < vertices.getLength(); i++) {
+    var xy = vertices.getAt(i);
+    contentString += '<br>' + 'Coordinate ' + i + ':<br>' + xy.lat() + ',' +
+        xy.lng();
+  }
+
+  // Replace the info window's content and position.
+  infoWindow.setContent(contentString);
+  infoWindow.setPosition(event.latLng);
+
+  infoWindow.open(map);
+}
